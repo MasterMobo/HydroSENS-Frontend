184e3ffd1d8d78da59a7032a8222563855e90601
MasterMobo <mastermobo101@gmail.com>
2025-05-13 16:50:57 +0700

    chore: changed dashboard color

--- src/pages/MapPage/components/RegionDashboard.tsx
+++ src/pages/MapPage/components/RegionDashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function RegionDashboard() {
     return (
-        <div className="absolute top-0 right-0 w-[50vw] h-full bg-white">
+        <div className="absolute top-0 right-0 w-[50vw] h-full bg-gray-100">
             RegionDashboard
         </div>
     );
