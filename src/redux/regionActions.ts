b51ec0298cc1db3896dac82fd28d8b250e68bd9d
MasterMobo <mastermobo101@gmail.com>
2025-05-16 22:59:39 +0700

    feat: added delete on region list

--- src/redux/regionActions.ts
+++ src/redux/regionActions.ts
@@ -1,9 +1,15 @@
 // Action Types
 export const SELECT_REGION = "SELECT_REGION";
+export const DELETE_REGION = "DELETE_REGION";
 
 // Action Creators
 export const selectRegion = (regionIndex: number | null) => ({
     type: SELECT_REGION,
+    payload: regionIndex,
+});
+
+export const deleteRegion = (regionIndex: number) => ({
+    type: DELETE_REGION,
     payload: regionIndex,
 });
 
@@ -13,4 +19,9 @@ export interface SelectRegionAction {
     payload: number | null;
 }
 
-export type RegionActionTypes = SelectRegionAction;
+export interface DeleteRegionAction {
+    type: typeof DELETE_REGION;
+    payload: number;
+}
+
+export type RegionActionTypes = SelectRegionAction | DeleteRegionAction;
