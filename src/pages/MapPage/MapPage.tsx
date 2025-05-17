91577c72212cc54b87654a13dd16a6a4133fb24f
Alak <hoang2504.rmit@gmail.com>
2025-05-17 14:08:32 +0700

    chore: added dashboard transitions

--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -1,24 +1,45 @@
 import React from "react";
 import "leaflet/dist/leaflet.css";
-import RegionList from "./components/RegionList";
-import LeafletMap from "./components/LeafletMap";
-import UiOverlay from "./components/UiOverlay";
+
+import RegionList   from "./components/RegionList";
+import LeafletMap   from "./components/LeafletMap";
 import RegionDashboard from "./components/RegionDashboard";
+
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
-function MapPage() {
-    const { selectedRegionIndex } = useSelector(
-        (state: RootState) => state.regionState
-    );
+import { AnimatePresence, motion } from "framer-motion";
 
-    return (
-        <div className="relative h-screen w-full">
-            <LeafletMap />
-            <RegionList />
-            {selectedRegionIndex != null ? <RegionDashboard /> : null}
-        </div>
-    );
+function MapPage() {
+  const { selectedRegionIndex } = useSelector(
+    (state: RootState) => state.regionState
+  );
+
+  return (
+    <div className="relative h-screen w-full overflow-hidden">
+      {/* left-hand map / polygons */}
+      <LeafletMap />
+
+      {/* region list (left sidebar) */}
+      <RegionList />
+
+      {/* right-hand dashboard */}
+      <AnimatePresence>
+        {selectedRegionIndex != null && (
+          <motion.div
+            key="dashboard"                     /* keeps component mounted when region changes */
+            initial={{ x: "100%" }}             /* start just off-screen right */
+            animate={{ x: 0 }}                  /* slide in */
+            exit={{ x: "100%" }}                /* slide out when Back sets index to null */
+            transition={{ type: "tween", duration: 0.35 }}
+            className="absolute inset-y-0 right-0 w-[50vw]" /* wrapper that slides */
+          >
+            <RegionDashboard />                 {/* dashboard content stays mounted */}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 }
 
 export default MapPage;
