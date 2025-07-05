d75fcf9609028f9d5a06d0b862c560c9c9074f60
MasterMobo <mastermobo101@gmail.com>
2025-07-05 15:22:03 +0700

    fix: changed results to outputs

--- src/api/hydrosens.ts
+++ src/api/hydrosens.ts
@@ -2,8 +2,8 @@ import { HydrosensResponse, HydrosensOutputs } from "@/types/hydrosens";
 import { api } from "./client";
 
 export async function postHydrosens(payload: any): Promise<HydrosensOutputs> {
-  const { data } = await api.post<HydrosensResponse>("/analyze", payload, {
-    headers: { "Content-Type": "application/json" },
-  });
-  return data.results;
+    const { data } = await api.post<HydrosensResponse>("/analyze", payload, {
+        headers: { "Content-Type": "application/json" },
+    });
+    return data.outputs;
 }
